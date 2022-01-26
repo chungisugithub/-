73293034a6729e393cf60b3d19952508d77a552c
chungisugithub <chungisu@yahoo.com>
2022-01-26 17:12:02 +0900

    Update TestConnection.java

--- TestConnection.java
+++ TestConnection.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TestConnection {
-
+ // '김행복'의 총매출 구하기
 	private static Connection getConnection() throws Exception, SQLException {
 		Class.forName("oracle.jdbc.OracleDriver");
 		Connection con = DriverManager.getConnection(
@@ -13,6 +13,20 @@ public class TestConnection {
 		return con;
 	}
 
+	private static int getKimsID() throws SQLException, Exception {
+		// 데이터 베이스 테이블에 아이디 정보가 있다.
+		// select custno from member_tbl_02 where custname = '김행복';
+		int custno = 0;
+		String query = "select custno from member_tbl_02 where custname = '이축복'";
+		ResultSet result;
+		Connection con = getConnection();
+		result = con.prepareStatement(query).executeQuery();
+		while (result.next()) {
+			custno = result.getInt("CUSTNO");
+			System.out.println("custno: " + custno);
+		}
+		return custno;
+	}
 	public static void main(String[] args) throws Exception {
 		int sum = 0;int custno = 0;	int price = 0;
 		String query = "select custno, price from money_tbl_02";
@@ -22,13 +36,12 @@ public class TestConnection {
 
 		while (result.next()) {
 			custno = result.getInt("CUSTNO");
-			if (custno != 10001) continue;
+			if (custno != getKimsID()) continue;
 			price = result.getInt("PRICE");
 			sum += price;
 			System.out.println("custno: " + custno + " price: " + price);
 		}
 		System.out.println("합계는 " + sum + " 입니다.");
 	}
-	
 
 }
