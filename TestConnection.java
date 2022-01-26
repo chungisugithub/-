5d5f74eadbfab69be3837d64a893ddac97bceb19
chungisugithub <chungisu@yahoo.com>
2022-01-26 16:53:15 +0900

    Update TestConnection.java

--- TestConnection.java
+++ TestConnection.java
@@ -3,28 +3,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 public class TestConnection {
 
-	public static void main(String[] args) throws Exception {
-		
-		String query = "select * from employees"
-				+ " where first_name = 'Donald'";
-		ResultSet result;
-			
-		Connection con = getConnection();
-		
-		result = con.prepareStatement(query).executeQuery();
-		
-		while(result.next()) {
-			System.out.println(result.getString("first_name"));
-		}
-	}
-
-	private static Connection getConnection() throws Exception,
-			SQLException {
+	private static Connection getConnection() throws Exception, SQLException {
 		Class.forName("oracle.jdbc.OracleDriver");
 		Connection con = DriverManager.getConnection(
-					"jdbc:oracle:thin:@//localhost:1521/xe","hr","hr");
+				"jdbc:oracle:thin:@//localhost:1521/xe", "shop", "1234");
 		return con;
 	}
+
+	public static void main(String[] args) throws Exception {
+		int sum = 0;int custno = 0;	int price = 0;
+		String query = "select custno, price from money_tbl_02";
+		ResultSet result;
+		Connection con = getConnection();
+		result = con.prepareStatement(query).executeQuery();
+
+		while (result.next()) {
+			custno = result.getInt("CUSTNO");
+			if (custno != 10001) continue;
+			price = result.getInt("PRICE");
+			sum += price;
+			System.out.println("custno: " + custno + " price: " + price);
+		}
+		System.out.println("합계는 " + sum + " 입니다.");
+	}
+	
+
 }
